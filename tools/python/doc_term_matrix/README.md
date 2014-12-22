b934109b7f775117395f2e3c402f4081e141b12b
Your Name <detzold@gmail.com>
2014-12-22 15:06:31 +0100

    reorg

---
+++ tools/python/doc_term_matrix/README.md
@@ -0,0 +1,3 @@
+This directory contains a script which can be used to create a document-term matrix and store the matrix in a format that can be read with MATLAB or Octave.
+
+
---
+++ tools/python/doc_term_matrix/doc_term_matrix.py
@@ -0,0 +1,33 @@
+import sys
+
+import numpy as np
+import scipy.io
+from array import array
+
+def doc_term_matrix(filename):
+	mapping = {}
+	docs = []
+	for i in open(filename):
+		words = i.strip().split()
+		d = []
+		for w in words:
+			if not w in mapping:
+				mapping[w] = len(mapping)
+			d.append(mapping[w])
+		docs.append(d)
+	m = np.zeros((len(docs), len(mapping)))
+	for i, d in enumerate(docs):
+		v = np.array([0] * len(mapping))
+		for w in d:
+			v[w] += 1
+		m[i] = v
+	return m
+
+def save_matrix_as_matlab(fname, name, matrix):
+	scipy.io.savemat(fname, {name: matrix}, oned_as = 'column', do_compression = True)
+
+
+if __name__ == "__main__":
+	m = doc_term_matrix("example.txt");
+	print m
+	save_matrix_as_matlab("example.mat", "docterm", m);
