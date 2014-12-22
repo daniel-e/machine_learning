f1946838b30e5e3835e24304f5f6cfb077215a85
Your Name <detzold@gmail.com>
2014-12-22 16:15:32 +0100

    quotes

--- octave/examples/ex_linear_regression.m
+++ octave/examples/ex_linear_regression.m
@@ -5,7 +5,7 @@ addpath('../../data/synthetic');
 
 % ---------- create the dataset ----------
 % seed the random number generator with a constant to get reproducable results
-rand("seed", 17);
+rand('seed', 17);
 % n = number of examples
 n = 50;
 data = createData2(n);
@@ -20,19 +20,19 @@ figure('Position', [0, 0, 900, 400]);
 % plot the data points
 subplot(1, 2, 1);
 hold on;
-plot(X(:,2), y, "xo", "markersize", 8, "linewidth", 2, "markeredgecolor", "black", "markerfacecolor", "yellow");
-title("input data");
+plot(X(:,2), y, 'xo', 'markersize', 8, 'linewidth', 2, 'markeredgecolor', 'black', 'markerfacecolor', 'yellow');
+title('input data');
 grid on;
 % plot 
-plot([0, 1], [1, 0; 1, 1] * theta, "linewidth", 2);
+plot([0, 1], [1, 0; 1, 1] * theta, 'linewidth', 2);
 
 % plot error on training set after each iteration
 subplot(1, 2, 2);
 n = length(cost_history);
-plot(1:n, cost_history, "linewidth", 2);
-title("Error on training set");
-xlabel("number of iterations")
-ylabel("error");
+plot(1:n, cost_history, 'linewidth', 2);
+title('Error on training set');
+xlabel('number of iterations')
+ylabel('error');
 grid on;
 
 % create png of plot
