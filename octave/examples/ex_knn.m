755dd6655402cd5ff598b92e3eea505692676ec8
Your Name <detzold@gmail.com>
2014-12-22 15:42:48 +0100

    doc

--- octave/examples/ex_knn.m
+++ octave/examples/ex_knn.m
@@ -3,18 +3,18 @@
 % This example shows how to classify handwritten digits with a simple
 % kNN classifier.
  
-addpath("..")
+addpath('..')
 
 more off;
-disp("Loading MNIST dataset ...");
+disp('Loading MNIST dataset ...');
 load('../../data/mnist/mnist.mat');
 
 n = 50;
-printf("classifying %d test examples...\n", n);
+printf('classifying %d test examples...\n', n);
 X = testingX(1:n,:);
 y = testingY(1:n,:);
 k = 5;
 [accuracy, predictions] = testClassifier(@(p) knn(trainingX, trainingY, p, k), X, y);
 
-printf("accuracy = %f\n", accuracy)
+printf('accuracy = %f\n', accuracy)
 
