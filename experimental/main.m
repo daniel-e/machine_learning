1827761e6cf15574037524653cbdf106fb9d938a
Your Name <detzold@gmail.com>
2015-03-27 13:34:15 +0100

    create animation

--- experimental/main.m
+++ experimental/main.m
@@ -1,6 +1,9 @@
 1;
 
 more off;
+hold on;
+clf;
+
 rand('seed', 123);  % seed prng to get reproducible results
 
 npop = 100;         % size of initial population
@@ -26,15 +29,28 @@ for iter = 1:niters
 		Pn = [Pn; createOffsprings(P(select(s), :), P(select(s), :))];
 	end
 	P = mutatePopulation(Pn, mutationRate);
+
+
+	% plot the best solution of the current generation
+	subplot(1, 2, 1);
+	best = X(P(s(1, 2), :), :);
+	best = [best; best(1, :)];
+	plot(best(:, 1), best(:, 2), '-o', 'linewidth', 2, 'markerfacecolor', 'b');
+	title('best solution');
+	axis("off");
+	refresh();
+
+	% plot the average fitness vs generation
+	subplot(1, 2, 2);
+	plot(J(1:iter), 'linewidth', 2);
+	xlabel('iteration');
+	title('fitness');
+	xlim([0 niters]);
+	grid on;
+	refresh();
+
+	%filename = sprintf('/tmp/image%05d.png', iter);
+	%print (filename, '-dpng', '-S640,400');
+	% avconv -framerate 10 -i 'image%05d.png' -c:v libx264 -r 30 -crf 4 -pix_fmt yuv420p out.mp4
 end
 
-% plot:
-% 1) the result, i.e. the path of the best solution in the last generation
-subplot(1, 2, 1);
-best = X(P(s(1, 2), :), :);
-best = [best; best(1, :)];
-plot(best(:, 1), best(:, 2), 'linewidth', 2);
-
-% 2) the average fitness vs generation
-subplot(1, 2, 2);
-plot(J, 'linewidth', 2);
